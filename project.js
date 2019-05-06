602851c51eb60206b556e4b480c1b99743ad4727
mariammaher11 <47921154+mariammaher11@users.noreply.github.com>
2019-05-06 15:19:53 +0200

    Create project.js

---
+++ project.js
@@ -0,0 +1,24 @@
+/* <table>
+                    <tr>
+                        <td> <img src=".jpg" width="100%" height="300px"> </td>
+                        <td> <img src=".jpg" width="100%" height="300px"> </td>
+                        <td> <img src=".jpg" width="100%" height="300px"> </td>
+                        <td> <img src=".jpg" width="100%" height="300px"> </td>
+                    </tr>
+                    <tr>
+                        <th><a href="" target="_blank"> Mahmoud Diab </a></th>
+                        <th><a href="" target="_blank">Ahmed Abo Zaid</a></th>
+                        <th><a href="" target="_blank">Mohamed Zakzuk</a></th>
+                        <th><a href="" target="_blank">Mostafa Makram</a></th>
+                        
+                    </tr>
+    
+                </table>
+*/
+
+function change(m){
+    m.style.backgroundColor="rgba(186, 186, 206, 0.534)";
+}
+function back(m){
+    m.style.backgroundColor="rgba(20, 20, 20, 0.863)";
+}
